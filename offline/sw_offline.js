16b6c6128658b465bddee04135652c41e3fda7d7
Samuel <samuel@libertyjayaone.local>
2018-11-13 07:10:00 +0700

    update offline

---
+++ offline/sw_offline.js
@@ -0,0 +1,11 @@
+﻿if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        var code = getCode() == undefined ? '' : '?code=' + getCode();
+        var guid = getGUID() == undefined ? '' : '&guid=' + getGUID();
+
+        navigator.serviceWorker
+            .register('../../../sw_offline.aspx' + code + guid)
+            .then(function (reg) { console.log('Service Worker: Registered') })
+            .catch(function (err) { console.log('Service Worker: Error: ' + err) })
+    });
+}
