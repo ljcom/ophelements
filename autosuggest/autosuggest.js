6e40ff4fc95dc6ce927e0d1e4014679b74967a33
Samuel <samuel@libertyjayaone.local>
2018-11-29 11:09:31 +0700

    update edit and remove button

--- autosuggest/autosuggest.js
+++ autosuggest/autosuggest.js
@@ -25,61 +25,81 @@ function autosuggest_onchange(ini, flag, code, GUID, formid) {
     }
 }
 
-function autosuggestSetValue(deferreds, SelectID, code, colKey, defaultValue, wf1, wf2) { 
-	autosuggest_setValue(deferreds, SelectID, code, colKey, defaultValue, wf1, wf2); } //old version
+function autosuggestSetValue(deferreds, SelectID, code, colKey, defaultValue, wf1, wf2) {
+    autosuggest_setValue(deferreds, SelectID, code, colKey, defaultValue, wf1, wf2);
+} //old version
 
 function autosuggest_setValue(deferreds, SelectID, code, colKey, defaultValue, wf1, wf2) {
     //default value only
     if (wf1 == '' || wf1 == undefined) wf1 = 'wf1isnone';
     if (wf2 == '' || wf2 == undefined) wf2 = 'wf2isnone';
-    if (defaultValue != '' && defaultValue  != undefined) {
-		if (deferreds) {
-			deferreds.push($.ajax({
-				url: "OPHCORE/api/msg_autosuggest.aspx",
-				data: {
-					code: code,
-					colkey: colKey,
-					defaultValue: defaultValue,
-					wf1value: ($("#" + wf1).data("value") === undefined ? "" : $("#" + wf1).data("value")),
-					wf2value: ($("#" + wf2).data("value") === undefined ? "" : $("#" + wf2).data("value")),
+    if (defaultValue != '' && defaultValue != undefined) {
+        if (deferreds) {
+            deferreds.push($.ajax({
+                url: "OPHCORE/api/msg_autosuggest.aspx",
+                data: {
+                    code: code,
+                    colkey: colKey,
+                    defaultValue: defaultValue,
+                    wf1value: ($("#" + wf1).data("value") === undefined ? "" : $("#" + wf1).data("value")),
+                    wf2value: ($("#" + wf2).data("value") === undefined ? "" : $("#" + wf2).data("value")),
                     parentCode: getCode()
-				},
-				dataType: "json",
-				success: function (data) {
-					var newOption = new Option(data.results[0].text, data.results[0].id, true, true);
-					var InitialValue = data.results[0].id;
-					$("#" + SelectID).data("old", InitialValue);
-					$("#" + SelectID).val(InitialValue);
-					$("#" + SelectID).data("oldtext", data.results[0].text);
-					$("#" + SelectID).append(newOption).trigger('change');
+                },
+                dataType: "json",
+                success: function (data) {
+                    var newOption = new Option(data.results[0].text, data.results[0].id, true, true);
+                    var InitialValue = data.results[0].id;
+                    $("#" + SelectID).data("old", InitialValue);
+                    $("#" + SelectID).val(InitialValue);
+                    $("#" + SelectID).data("oldtext", data.results[0].text);
+                    $("#" + SelectID).append(newOption).trigger('change');
 
-				}
-			}));
-		}
+                    $selection = $("#select2-" + SelectID + "-container").parents('.selection');
+                    if ($selection.children('#removeForm' + SelectID).length === 0)
+                        $('#removeForm' + SelectID).appendTo($selection);
+                    $('#removeForm' + SelectID).show();
+
+                    $selection = $("#select2-" + SelectID + "-container").parents('.selection');
+                    if ($selection.children('#editForm' + SelectID).length === 0)
+                        $('#editForm' + SelectID).appendTo($selection);
+                    $('#editForm' + SelectID).show();
+                }
+            }));
+        }
         else {
 
-			$.ajax({
-				url: "OPHCORE/api/msg_autosuggest.aspx",
-				data: {
-					code: code,
-					colkey: colKey,
-					defaultValue: defaultValue,
-					wf1value: ($("#" + wf1).data("value") === undefined ? "" : $("#" + wf1).data("value")),
-					wf2value: ($("#" + wf2).data("value") === undefined ? "" : $("#" + wf2).data("value")),
+            $.ajax({
+                url: "OPHCORE/api/msg_autosuggest.aspx",
+                data: {
+                    code: code,
+                    colkey: colKey,
+                    defaultValue: defaultValue,
+                    wf1value: ($("#" + wf1).data("value") === undefined ? "" : $("#" + wf1).data("value")),
+                    wf2value: ($("#" + wf2).data("value") === undefined ? "" : $("#" + wf2).data("value")),
                     parentCode: getCode()
-				},
-				dataType: "json",
-				success: function (data) {
-					var newOption = new Option(data.results[0].text, data.results[0].id, true, true);
-					var InitialValue = data.results[0].id;
-					$("#" + SelectID).data("old", InitialValue);
-					$("#" + SelectID).val(InitialValue);
-					$("#" + SelectID).data("oldtext", data.results[0].text);
-					$("#" + SelectID).append(newOption).trigger('change');
+                },
+                dataType: "json",
+                success: function (data) {
+                    var newOption = new Option(data.results[0].text, data.results[0].id, true, true);
+                    var InitialValue = data.results[0].id;
+                    $("#" + SelectID).data("old", InitialValue);
+                    $("#" + SelectID).val(InitialValue);
+                    $("#" + SelectID).data("oldtext", data.results[0].text);
+                    $("#" + SelectID).append(newOption).trigger('change');
 
-				}
-			})
-		}
+                    $selection = $("#select2-" + SelectID + "-container").parents('.selection');
+                    if ($selection.children('#removeForm' + SelectID).length === 0)
+                        $('#removeForm' + SelectID).appendTo($selection);
+                    $('#removeForm' + SelectID).show();
+
+                    $selection = $("#select2-" + SelectID + "-container").parents('.selection');
+                    if ($selection.children('#editForm' + SelectID).length === 0)
+                        $('#editForm' + SelectID).appendTo($selection);
+                    $('#editForm' + SelectID).show();
+
+                }
+            })
+        }
     }
 
     //return aj;
